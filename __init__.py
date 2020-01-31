bc7be48c0d417e145e1415e824e942dfbd8457a4
pythona620 <59389520+pythona620@users.noreply.github.com>
2020-01-31 12:19:04 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -10,32 +10,18 @@ LOGGER = getLogger(__name__)
 class NumberGuessSkill(MycroftSkill):
 	def get_numerical_response(self, dialog):
 		while True:
-			val = self.get_response(dialog)
-			
+			val = self.get_response(dialog)		
 	@intent_handler(IntentBuilder("").require("plan").optionally("going").optionally("Suggest"))
 	def handle_start_game_intent(self, message):
 		self.speak_dialog("start")
-
 		# get lower bound
-		lowerBound = self.get_numerical_response("get.lower")
+		source = self.get_numerical_response("get.source")
 		# get upper bound
-		upperBound = self.get_numerical_response("get.upper")
-
+		destination = self.get_numerical_response("get.destination")
 		answer = self.speak(lowerBound, upperBound)
-# 		userGuess = lowerBound - 1
-# 		while userGuess != answer:
-# 			userGuess = self.get_numerical_response("guess")
-# 			if userGuess < answer:
-# 				self.speak_dialog("too.low")
-# 			elif userGuess > answer:
-# 				self.speak_dialog("too.high")
-# 		self.speak_dialog("correct")
+
 
 	def stop(self):
-# 		lowerBound, upperBound = 0, 100
-# 		answer = 0
-# 		userGuess = answer
-# 		return True
 		pass
 
 def create_skill():
