a614f02cacc80d9e4661de2fb3f1a7db096fd4ee
pythona620 <59389520+pythona620@users.noreply.github.com>
2020-01-31 12:11:33 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,51 +4,39 @@ from mycroft.util.log import LOG, getLogger
 
 from random import randint
 
-__author__ = 'Arc676/Alessandro Vinciguerra'
+__author__ = 'prasad'
 LOGGER = getLogger(__name__)
 
 class NumberGuessSkill(MycroftSkill):
-
-	lowerBound = 0
-	upperBound = 100
-	answer = 0
-	userGuess = 0
-
 	def get_numerical_response(self, dialog):
 		while True:
 			val = self.get_response(dialog)
-			try:
-				val = int(val)
-				return val
-			except ValueError:
-				self.speak_dialog("invalid.input")
-			except:
-				self.speak_dialog("input.error")
-
-	@intent_handler(IntentBuilder("").require("NumberGuess").optionally("Play").optionally("Suggest"))
+			
+	@intent_handler(IntentBuilder("").require("plan").optionally("going").optionally("Suggest"))
 	def handle_start_game_intent(self, message):
-		self.speak_dialog("start.game")
+		self.speak_dialog("start")
 
 		# get lower bound
 		lowerBound = self.get_numerical_response("get.lower")
 		# get upper bound
 		upperBound = self.get_numerical_response("get.upper")
 
-		answer = randint(lowerBound, upperBound)
-		userGuess = lowerBound - 1
-		while userGuess != answer:
-			userGuess = self.get_numerical_response("guess")
-			if userGuess < answer:
-				self.speak_dialog("too.low")
-			elif userGuess > answer:
-				self.speak_dialog("too.high")
-		self.speak_dialog("correct")
+		answer = self.speak(lowerBound, upperBound)
+# 		userGuess = lowerBound - 1
+# 		while userGuess != answer:
+# 			userGuess = self.get_numerical_response("guess")
+# 			if userGuess < answer:
+# 				self.speak_dialog("too.low")
+# 			elif userGuess > answer:
+# 				self.speak_dialog("too.high")
+# 		self.speak_dialog("correct")
 
 	def stop(self):
-		lowerBound, upperBound = 0, 100
-		answer = 0
-		userGuess = answer
-		return True
+# 		lowerBound, upperBound = 0, 100
+# 		answer = 0
+# 		userGuess = answer
+# 		return True
+		pass
 
 def create_skill():
 	return NumberGuessSkill()
